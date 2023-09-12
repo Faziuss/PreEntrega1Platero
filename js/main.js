dfc026ceb50b3dc5001a4a3a0dc87f37f0934dd5
Fabrizio Platero <fabriplatero88@gmail.com>
2023-09-12 02:16:54 -0300

    added prompt to each case with 2 different currencies to pick

--- js/main.js
+++ js/main.js
@@ -19,10 +19,28 @@ function conversorMoneda() {
 
   switch (entrada) {
     case 1:
+        let usdAmoneda = parseInt(
+            prompt(
+              "CONVERSOR DE MONEDAS: Elige la moneda a convertir (Ingresa con los respectivos numeros)\n1. ARS\n2. EURO\n3. SALIR"
+            )
+          );
+        
       break;
     case 2:
+        let arsAmoneda = parseInt(
+            prompt(
+              "CONVERSOR DE MONEDAS: Elige la moneda a convertir (Ingresa con los respectivos numeros)\n1. USD\n2. EURO\n3. SALIR"
+            )
+          );
+        
       break;
     case 3:
+        let euroAmoneda = parseInt(
+            prompt(
+              "CONVERSOR DE MONEDAS: Elige la moneda a convertir (Ingresa con los respectivos numeros)\n1. USD\n2. ARS\n3. SALIR"
+            )
+          );
+        
       break;
     case 4:
     ejecutar = false;

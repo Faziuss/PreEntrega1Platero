fde2da761ba6d4655f15bdfcb6e99c379dc77029
Fabrizio Platero <fabriplatero88@gmail.com>
2023-09-12 02:04:23 -0300

    added function and switch system

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
-let entrada = prompt("CONVERSOR DE MONEDAS: Elige una moneda (Ingresa con los respectivos numeros)\n1. USD\n2. ARS\n3. EURO")
+function conversorMoneda() {
+  let entrada = parseInt(
+    prompt(
+      "CONVERSOR DE MONEDAS: Elige una moneda (Ingresa con los respectivos numeros)\n1. USD\n2. ARS\n3. EURO\n4. SALIR"
+    )
+  );
 
+  if (entrada === null || entrada === "") {
+    alert("Moneda invalida, elige una moneda valida.");
+    moneda1();
+    return;
+  }
+
+  switch (entrada) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+
+    default:
+      alert("Opcion invalida!");
+      break;
+  }
+
+  conversorMoneda();
+}
+
+conversorMoneda();

3e1d70179946fd50bdae2ea1c2f67b1d8dec5922
Fabrizio Platero <fabriplatero88@gmail.com>
2023-09-28 12:22:51 -0300

    added agregarMoneda function

--- js/main.js
+++ js/main.js
@@ -82,6 +82,35 @@ function convertirMoneda() {
   }
 }
 
+function agregarMoneda(){
+  const promptMoneda = prompt("Ingrese el nombre de la nueva moneda:");
+  const nameMoneda = promptMoneda.toUpperCase();
+  if (!nameMoneda) {
+    alert("El nombre de la moneda no puede estar vacío.");
+    return;
+  }
+
+  const tasas = {}
+
+  monedas.forEach((moneda) => {
+    const tasa = parseFloat(prompt(`Ingresa la tasa de conversión de ${nameMoneda} a ${moneda.name}:`));
+    if (isNaN(tasa)) {
+      alert("Error, Ingresa un numero valido");
+      return;
+    }
+    tasas[moneda.name] = tasa;
+  });
+
+  console.log("tasas nueva moneda", tasas)
+
+  const nuevaMoneda = { name: nameMoneda, rate: tasas };
+  monedas.push(nuevaMoneda);
+
+  console.log("objeto neuva moneda",nuevaMoneda)
+
+  alert(`${nameMoneda} ha sido agregada con éxito.`);
+}
+
 while (ejecutar) {
   const textoPrompt =
     "Elige una opcion (Ingresa con los respectivos numeros)\n1. Convertir Monedas\n2. Agregar Moneda\n3. SALIR";
@@ -106,106 +135,5 @@ while (ejecutar) {
     default:
       alert("Opcion invalida, elige una opcion valida.");
   }
-
-  /* switch (entrada) {
-    case 1:
-      let resultadoUSD;
-
-      const montoUSD = parseFloat(prompt("Ingresa el monto (USD)"));
-      if (isNaN(montoUSD)) {
-        alert("Monto Invalido!, ingresa un número válido.");
-        break;
-      }
-      const usdAmoneda = parseInt(
-        prompt(
-          "CONVERSOR DE MONEDAS: Elige la moneda a convertir (Ingresa con los respectivos numeros)\n1. ARS\n2. EURO"
-        )
-      );
-
-      if (usdAmoneda != 1 && usdAmoneda != 2) {
-        alert("Opcion Invalida!");
-      } else if (usdAmoneda === 1) {
-        resultadoUSD = montoUSD * usdToArs;
-        alert(
-          `${montoUSD} dolares equivalen a ${resultadoUSD} pesos argentinos`
-        );
-      } else {
-        resultadoUSD = montoUSD * usdToEuro;
-        alert(`${montoUSD} dolares equivalen a ${resultadoUSD} euros`);
-      }
-
-      break;
-    case 2:
-      let resultadoARS;
-
-      const montoARS = parseFloat(prompt("Ingresa el monto (ARS)"));
-      if (isNaN(montoARS)) {
-        alert("Monto Invalido!, ingresa un número válido.");
-        break;
-      }
-      const arsAmoneda = parseInt(
-        prompt(
-          "CONVERSOR DE MONEDAS: Elige la moneda a convertir (Ingresa con los respectivos numeros)\n1. USD\n2. EURO"
-        )
-      );
-
-      if (arsAmoneda != 1 && arsAmoneda != 2) {
-        alert("Opcion Invalida!");
-      } else if (arsAmoneda === 1) {
-        resultadoARS = montoARS * arsToUsd;
-        alert(
-          `${montoARS} pesos argentinos equivalen a ${resultadoARS.toFixed(
-            2
-          )} dolares`
-        );
-      } else {
-        resultadoARS = montoARS * arsToEuro;
-        alert(
-          `${montoARS} pesos argentinos equivalen a ${resultadoARS.toFixed(
-            2
-          )} euros`
-        );
-      }
-
-      break;
-    case 3:
-      let resultadoEUR;
-
-      const montoEURO = parseFloat(prompt("Ingresa el monto (EUR)"));
-      if (isNaN(montoEURO)) {
-        alert("Monto Invalido!, ingresa un número válido.");
-        break;
-      }
-
-      const euroAmoneda = parseInt(
-        prompt(
-          "CONVERSOR DE MONEDAS: Elige la moneda a convertir (Ingresa con los respectivos numeros)\n1. USD\n2. ARS"
-        )
-      );
-
-      if (euroAmoneda != 1 && euroAmoneda != 2) {
-        alert("Opcion Invalida!");
-      } else if (euroAmoneda === 1) {
-        resultadoEUR = montoEuro * euroToUsd;
-        alert(
-          `${montoEURO} euros equivalen a ${resultadoEUR.toFixed(2)} dolares`
-        );
-      } else {
-        resultadoEUR = montoEURO * euroToArs;
-        alert(
-          `${montoEURO} euros equivalen a ${resultadoEUR.toFixed(
-            2
-          )} pesos argentinos`
-        );
-      }
-
-      break;
-    case 4:
-      ejecutar = false;
-      break;
-
-    default:
-      alert("Opcion invalida!");
-      break;
-  } */
+  
 }

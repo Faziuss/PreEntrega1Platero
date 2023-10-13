439c08948393701aa341d675ecd51e5e87f4eaed
Fabrizio Platero <fabriplatero88@gmail.com>
2023-10-12 22:03:11 -0300

    Add dynamic currency converter with modal for adding new currencies

--- js/main.js
+++ js/main.js
@@ -1,220 +1,211 @@
-const $input1 = document.getElementById("input1")
-const $input2 = document.getElementById("input2")
-const $select1 = document.getElementById("select1")
-const $select2 = document.getElementById("select2")
-
-// 
-const data = [
-    {name: "USD", rate: {ARS: 349.96, EURO: 0.0027}},
-    {name: "ARS", rate: {USD:1/349.96, EURO: 0.0027}},
-    {name: "EURO", rate: {ARS: 375.85, USD: 1.07}}
-]
+const $input1 = document.getElementById("input1");
+const $input2 = document.getElementById("input2");
+const $select1 = document.getElementById("select1");
+const $select2 = document.getElementById("select2");
 
 //
-let currencies = JSON.parse(localStorage.getItem('currencies')) || []
+const data = [
+  { name: "USD", rate: { ARS: 349.96, EURO: 0.0027 } },
+  { name: "ARS", rate: { USD: 1 / 349.96, EURO: 0.0027 } },
+  { name: "EURO", rate: { ARS: 375.85, USD: 1.07 } },
+];
+
+//
+let currencies = JSON.parse(localStorage.getItem("currencies")) || [];
 
 //
 const main = () => {
-  if(!currencies.length){
-    localStorage.setItem("currencies", JSON.stringify(data))
-    currencies = data
+  if (!currencies.length) {
+    localStorage.setItem("currencies", JSON.stringify(data));
+    currencies = data;
+  }
+};
+
+main();
+
+const updateFirstInput = (newValue) => {
+  const currency2 = $select2.value;
+  const moneda = currencies.find((moneda) => {
+    return moneda.name === currency2;
+  });
+  const currency1 = $select1.value;
+
+  if (currency1 === currency2) {
+    $input1.value = newValue;
+  } else {
+    $input1.value = newValue * moneda.rate[currency1];
+  }
+};
+
+const updateSecondInput = (newValue) => {
+  const currency1 = $select1.value;
+  const moneda = currencies.find((moneda) => {
+    return moneda.name === currency1;
+  });
+  const currency2 = $select2.value;
+
+  if (currency1 === currency2) {
+    $input2.value = newValue;
+  } else {
+    $input2.value = newValue * moneda.rate[currency2];
+  }
+};
+
+$input1.addEventListener("input", (e) => {
+  updateSecondInput(e.target.value);
+});
+
+$input2.addEventListener("input", (e) => {
+  updateFirstInput(e.target.value);
+});
+
+$select1.addEventListener("change", () => {
+  updateSecondInput($input1.value);
+});
+
+$select2.addEventListener("change", () => {
+  updateSecondInput($input2.value);
+});
+
+function loadSelects() {
+  for (const currency of currencies) {
+    const $newOption = document.createElement("option")
+    $newOption.value = currency.name
+    $newOption.textContent = currency.name
+    const $clonedOption = $newOption.cloneNode(true)
+    $select1.appendChild($newOption)
+    $select2.appendChild($clonedOption)
   }
 }
 
-main()
-
-const updateFirstInput = (newValue) => {
-    const currency2 = $select2.value
-    const moneda = currencies.find((moneda)=>{return moneda.name === currency2})
-    const currency1 = $select1.value
-
-    if(currency1===currency2){
-        $input1.value = newValue
-    } else {
-        $input1.value = newValue * moneda.rate[currency1]
-    }
-}
-
-const updateSecondInput = (newValue) => {
-    const currency1 = $select1.value
-    const moneda = currencies.find((moneda)=>{return moneda.name === currency1})
-    const currency2 = $select2.value
-
-    if(currency1===currency2){
-        $input2.value = newValue
-    } else {
-        $input2.value = newValue * moneda.rate[currency2]
-    }
-}
-
-$input1.addEventListener("input", (e)=>{
-    updateSecondInput(e.target.value)
-})
-
-$input2.addEventListener("input", (e)=>{
-    updateFirstInput(e.target.value)
-})
-
-$select1.addEventListener("change", () => {
-    updateSecondInput($input1.value)
-})
-
-$select2.addEventListener("change", () => {
-    updateSecondInput($input2.value)
-})
-
 function generateCurrencyTable() {
-  const currencyTableDiv = document.getElementById('currencyTable');
-  const table = document.createElement('table');
-  table.classList.add('currency-table');
+  const currencyTableDiv = document.getElementById("currencyTable");
+  const table = document.createElement("table");
+  table.classList.add("currency-table");
 
-  const tableHeader = document.createElement('thead');
-  const headerRow = document.createElement('tr');
-  const headerName = document.createElement('th');
+  const tableHeader = document.createElement("thead");
+  const headerRow = document.createElement("tr");
+  const headerName = document.createElement("th");
   headerName.textContent = "Moneda";
   headerRow.appendChild(headerName);
 
   for (const currency of currencies) {
-      const headerRate = document.createElement('th');
-      headerRate.textContent = currency.name;
-      headerRow.appendChild(headerRate);
+    const headerRate = document.createElement("th");
+    headerRate.textContent = currency.name;
+    headerRow.appendChild(headerRate);
   }
 
   tableHeader.appendChild(headerRow);
   table.appendChild(tableHeader);
 
-  const tableBody = document.createElement('tbody');
+  const tableBody = document.createElement("tbody");
 
   for (const currency of currencies) {
-      const row = document.createElement('tr');
-      const currencyNameCell = document.createElement('td');
-      currencyNameCell.textContent = currency.name;
-      row.appendChild(currencyNameCell);
+    const row = document.createElement("tr");
+    row.id = `row${currency.name}`;
+    const currencyNameCell = document.createElement("td");
+    currencyNameCell.textContent = currency.name;
+    row.appendChild(currencyNameCell);
 
-      for (const otherCurrency of currencies) {
-          const rateCell = document.createElement('td');
-          rateCell.textContent = currency.rate[otherCurrency.name];
-          row.appendChild(rateCell);
-      }
+    for (const otherCurrency of currencies) {
+      const rateCell = document.createElement("td");
+      rateCell.textContent = currency.rate[otherCurrency.name];
+      row.appendChild(rateCell);
+    }
 
-      tableBody.appendChild(row);
+    tableBody.appendChild(row);
   }
 
   table.appendChild(tableBody);
-  currencyTableDiv.innerHTML = ''; 
+  currencyTableDiv.innerHTML = "";
   currencyTableDiv.appendChild(table);
 }
 
-document.addEventListener("DOMContentLoaded", generateCurrencyTable);
+document.addEventListener("DOMContentLoaded", ()=>{
+  generateCurrencyTable()
+  loadSelects()
+});
 
+const $openModal = document.getElementById("openModal");
+const $modal = document.getElementById("modal");
+const $closeModal = document.getElementById("closeModal");
+const $modalForm = document.getElementById("modalForm");
+const $ratesContainer = document.getElementById("ratesContainer");
 
+$openModal.addEventListener("click", () => {
+  $modal.style.display = "block";
+  currencies.forEach((currency) => {
+    addRateInput(currency.name);
+  });
+});
 
-/* let ejecutar = true;
+$closeModal.addEventListener("click", () => {
+  $modal.style.display = "none";
+});
 
-function listaMonedas() {
-  return monedas
-    .map((moneda, index) => `${index + 1}. ${moneda.name}`)
-    .join("\n");
+function addRateInput(currencyName) {
+  const rateLabel = document.createElement("label");
+  rateLabel.textContent = currencyName + ":";
+  const rateInput = document.createElement("input");
+  rateInput.type = "number";
+  rateInput.name = currencyName;
+  rateInput.id = currencyName;
+  $ratesContainer.appendChild(rateLabel);
+  $ratesContainer.appendChild(rateInput);
 }
 
-function listaMonedas2(monedas2) {
-  return monedas2
-    .map((moneda, index) => `${index + 1}. ${moneda.name}`)
-    .join("\n");
-}
-
-function convertirMoneda() {
-  const promptMonedaOrigen = prompt(
-    "Elige una moneda (Ingresa con los respectivos numeros)\n" + listaMonedas()
-  );
-  const monedaOrigen = parseInt(promptMonedaOrigen);
-  const origen = monedas.find((moneda, index) => index + 1 === monedaOrigen);
-
-  if (!origen) {
-    alert("Moneda invalida");
-    return;
-  }
-
-  const monedas2 = monedas.filter((moneda, index) => index + 1 != monedaOrigen);
-  const promptMonedaDestino = prompt(
-    "Elige a que moneda convertir (Ingresa con los respectivos numeros)\n" +
-      listaMonedas2(monedas2)
-  );
-  const monedaDestino = parseInt(promptMonedaDestino);
-  const destino = monedas2.find((moneda, index) => index + 1 === monedaDestino);
-
-  if (!destino) {
-    alert("Moneda invalida");
-    return;
-  }
-  const promptMonto = prompt("Ingresa un monto");
-  const monto = parseFloat(promptMonto);
-  if (isNaN(monto) || monto < 0) {
-    alert("Monto Invalido");
-    return;
-  }
-  const tasa = origen.rate[destino.name];
-
-  const resultado = monto * tasa;
-  alert(
-    `${monto} ${origen.name} equivalen a ${resultado.toFixed(2)} ${
-      destino.name
-    }.`
-  );
-}
-
-function agregarMoneda() {
-  const promptMoneda = prompt("Ingrese el nombre de la nueva moneda:");
-  const nameMoneda = promptMoneda.toUpperCase();
-  if (!nameMoneda) {
-    alert("El nombre de la moneda no puede estar vacío.");
-    return;
-  }
-
-  const tasas = {};
-
-  monedas.forEach((moneda) => {
-    const tasa = prompt(
-      `Ingresa la tasa de conversión de ${nameMoneda} a ${moneda.name}:`
-    );
-    const parsedTasa = parseFloat(tasa);
-    if (isNaN(parsedTasa)) {
-      alert("Error, Ingresa un numero valido");
-      return;
-    }
-    tasas[moneda.name] = parsedTasa;
-    moneda.rate[nameMoneda] = 1 / parsedTasa;
+$modalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newCurrency = { name: "", rate: {} };
+  const currencyName = document.getElementById("name").value;
+  newCurrency.name = currencyName.toUpperCase();
+  const inputElements = $ratesContainer.querySelectorAll("input");
+  inputElements.forEach((input, index) => {
+    const value = Number(input.value);
+    newCurrency.rate[input.name] = value;
+    currencies[index].rate[newCurrency.name] = 1 / value;
   });
 
-  const nuevaMoneda = { name: nameMoneda, rate: tasas };
-  monedas.push(nuevaMoneda);
+  const $newOption = document.createElement("option");
+  $newOption.value = newCurrency.name;
+  $newOption.textContent = newCurrency.name;
 
-  alert(`${nameMoneda} ha sido agregada con éxito.`);
-}
+  const $clonedOption = $newOption.cloneNode(true);
 
-while (ejecutar) {
-  const textoPrompt = prompt(
-    "Elige una opcion (Ingresa con los respectivos numeros)\n1. Convertir Monedas\n2. Agregar Moneda\n3. SALIR"
-  );
-  const entrada = parseInt(textoPrompt);
+  $select1.appendChild($newOption);
+  $select2.appendChild($clonedOption);
 
-  if (entrada === null || entrada === "") {
-    alert("Opcion invalida, elige una opcion valida.");
-    continue;
+  const column = document.createElement("th");
+  column.textContent = newCurrency.name;
+  const $headerRow = document.querySelector("thead tr");
+  $headerRow.appendChild(column);
+
+  
+  currencies.forEach((c) => {
+      const data = document.createElement("td");
+      data.textContent = c.rate[newCurrency.name];
+      const $row = document.querySelector(`#row${c.name}`);
+      $row.appendChild(data);
+  });
+
+  currencies.push(newCurrency);
+
+  const $tbody = document.querySelector("tbody")
+  const $newBodyRow = document.createElement("tr")
+  const $tdName = document.createElement("td")
+  $tdName.textContent = newCurrency.name
+  $newBodyRow.appendChild($tdName)
+
+  for (const currency of currencies) {
+    if(!currency.name !==newCurrency.name){
+      const $td = document.createElement("td")
+      $td.textContent = newCurrency.rate[currency.name]
+      $newBodyRow.appendChild($td)
+    }
   }
 
-  switch (entrada) {
-    case 1:
-      convertirMoneda();
-      break;
-    case 2:
-      agregarMoneda();
-      break;
-    case 3:
-      alert("Saliste del Conversor de Monedas.");
-      ejecutar = false;
-      break;
-    default:
-      alert("Opcion invalida, elige una opcion valida.");
-  }
-} */
+  $tbody.appendChild($newBodyRow)
+  
+  localStorage.setItem("currencies", JSON.stringify(currencies));
+  $modal.style.display = "none";
+});
